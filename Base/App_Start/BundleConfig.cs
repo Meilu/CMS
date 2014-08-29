3bcf3f3679ccd50a17b08da7697fc8792d03dde0
Jurgen Welschen <jurgen.welschen@gmail.com>
2014-08-30 00:54:58 +0200

    Squashed 'Web/' changes from ce10213..deca3b4
    deca3b4 Merge commit '35d125320226527129ab7cb4905ea93e67cb37d4'
    35d1253 Squashed 'Base/' changes from 56938bf..403c603
    b5aa858 Merge commit '5654422d48460cfbdb9d7715a9f33edebfc57d54'
    5654422 Squashed 'Base/' changes from 6af191a..56938bf
    30ebf80 added basescripts to bundleconfig
    1d35e10 Merge commit '9e4c290fd50d900967c642b10488b0a7e3516d51'
    9e4c290 Squashed 'Base/Content/' changes from 906b1c0..9d2d09e
    
    git-subtree-dir: Web
    git-subtree-split: deca3b4c8d61f62037759cde8ed90cb7bb4e4c9e

--- Base/App_Start/BundleConfig.cs
+++ Base/App_Start/BundleConfig.cs
@@ -6,7 +6,17 @@ namespace Base
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-          // register all of your bundles here
+          // register basic bundles here
+
+            bundles.Add(new StyleBundle("~/BaseStyles").Include(
+                 "~/Content/components/pure/pure.css",
+                 "~/Content/components/font-awesome/css/font-awesome.css"));
+
+            bundles.Add(new ScriptBundle("~/BaseScripts").Include(
+                 "~/Content/components/knockout/dist/knockout.js",
+                 "~/Content/components/jquery/dist/jquery.js",
+                 "~/Content/components/knockout-mapping/knockout.mapping.js"));
+
         }
     }
 }
